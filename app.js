63c632faa73213e73ca7d1004b5d52dff0d54198
Derek Hassick <derekhassick@Dereks-MacBook-Air.local>
2018-07-22 22:23:09 -0600

    spacing and semicolons

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 const diapers = require('./db/diapers');
-app.use(cors())
 
+app.use(cors())
 app.use(morgan('tiny'));
 app.use(bodyParser.json());
 
 
 app.get('/', (req, res) => {
-    res.json({ data: "congrats you've hit the api diapers" });
+    res.json({ data: "congrats you've hit the api diapers download out app to track your diaper changes" });
 });
 
 app.get('/api/diapers/:id', (req, res) => {
@@ -28,7 +28,7 @@ app.post('/api/diapers', (req, res) => {
   diapers.create(req.body)
   .then(diaper => {
     res.json({ diapers: diaper });
-  })
+  });
 });
 
 app.delete('/api/diapers/:id', (req, res) => {
@@ -36,33 +36,28 @@ app.delete('/api/diapers/:id', (req, res) => {
   res.status(200).json({ diapers: "successful deletion "})
 });
 
-app.put('/api/diapers/:id', (req, res) => {
-  console.log(req.body);
-  
+app.put('/api/diapers/:id', (req, res) => {  
   diapers.upDate(req.params.id, req.body)
-  .then( (diaper)=>res.status(200).json({diapers: diaper }) )
+  .then((diaper)=>res.status(200).json({diapers: diaper }));
 });
 
+app.use(notFound);
+app.use(errorHandler);
 
-app.use(notFound)
-app.use(errorHandler)
-
-// eslint-disable-next-line
 function notFound(req, res, next) {
-  const url = req.originalUrl
+  const url = req.originalUrl;
   if (!/favicon\.ico$/.test(url) && !/robots\.txt$/.test(url)) {
-    // Don't log less important (automatic) browser requests
-    console.error('[404: Requested file not found] ', url)
+    console.error('[404: Requested file not found] ', url);
   }
-  res.status(404).send({ error: 'Url not found', status: 404, url })
+  res.status(404).send({ error: 'Url not found', status: 404, url });
 }
 
-// eslint-disable-next-line
 function errorHandler(err, req, res, next) {
-  console.error('ERROR', err)
-  const stack = process.env.NODE_ENV !== 'production' ? err.stack : undefined
-  res.status(500).send({ error: err.message, stack, url: req.originalUrl })
+  console.error('ERROR', err);
+  const stack = process.env.NODE_ENV !== 'production' ? err.stack : undefined;
+  res.status(500).send({ error: err.message, stack, url: req.originalUrl });
 }
+
 app.listen(port)
   .on('error', console.error.bind(console))
-  .on('listening', console.log.bind(console, 'Listening on ' + port))
+  .on('listening', console.log.bind(console, 'Listening on ' + port));
