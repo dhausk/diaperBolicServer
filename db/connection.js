3593c9170252138700b07cebb28bc5d7b155570f
Derek Hassick <derekhassick@Dereks-MacBook-Air.local>
2018-07-24 15:48:07 -0600

    fixed typo

--- db/connection.js
+++ db/connection.js
@@ -2,4 +2,4 @@ const monk = require('monk');
 const connectionString = process.env.MONGODB_URI || process.env.MONGOLAB_URI || "localhost:27017/diapers";
 const db = monk(connectionString);
 db.then(()=> console.log('DB connected to server'));
-module.exports = db;
+module.exports = dbl
