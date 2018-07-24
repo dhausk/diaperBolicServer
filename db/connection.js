17dafe880f4f5fb15626a825252fb38d007d776c
Derek Hassick <derekhassick@Dereks-MacBook-Air.local>
2018-07-24 15:46:31 -0600

    added mladb heroku var

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,5 @@
 const monk = require('monk');
-const connectionString = process.env.MONGODB_URI || "localhost:27017/diapers";
+const connectionString = process.env.MONGODB_URI || process.env.MONGOLAB_URI || "localhost:27017/diapers";
 const db = monk(connectionString);
 db.then(()=> console.log('DB connected to server'));
 module.exports = db;
